440a511015d45c47f872ef9e81e8fae52e77e489
rossfadely <rossfadely@gmail.com>
2012-12-04 15:39:51 -0500

    simulator with good numbers

--- code/simulator.py
+++ code/simulator.py
@@ -1,53 +1,53 @@
 import numpy as np
 
+import time
+
 class FakeField(object):
 
     def __init__(self,flat,dark,
                  uninoise,
                  noisefact,
                  index = 2,
-                 Nrange=(300,1000),
+                 Nmean=3000,
                  fluxrange=(50,1000),
-                 psfsigma=1.5):
+                 psfsigma=1.5,
+                 window=4):
 
         self.psfsigma = psfsigma
-        self.N = np.random.rand() * (Nrange[1]-Nrange[0]) + \
-            Nrange[0]
+        self.N = np.random.poisson(Nmean)
         self.N = np.int(np.round(self.N))
         self.fluxrange = fluxrange
         self.uninoise = uninoise
         self.noisefact = noisefact
         self.index = index
+        self.window = window
 
         assert flat.shape == dark.shape, \
             'Flat and dark are not same shape'
+        self.shape = flat.shape
         self.flat = flat
         self.dark = dark
 
+        self.make_catalog()
         self.make_image()
 
 
-    def gaussianpsf(self,flux,xgrid,ygrid,x0,y0,psfsigma):
+    def add_gaussianpsfs(self):
         """
-        Return a gaussian psf on grid of pixels
+        Add gaussian psfs to image, this has been
+        tried as a broadcast but takes a lot of memory and time
         """
-        psf = flux * np.exp(-0.5 * ((xgrid-x0) ** 2 + (ygrid-y0) ** 2)
-                            / psfsigma ** 2) / np.sqrt(2. * np.pi * psfsigma ** 2)
-        return psf
 
-    def gaussianpsfs(self,xgrid,ygrid,x0,y0,psfsigma)
+        x, y = np.meshgrid(range(self.shape[0]),range(self.shape[1]))
 
-        # needs to go outside image
-        self.x0s = np.random.rand(self.N) * (self.shape[0]-1),
-        self.y0s = np.random.rand(self.N) * (self.shape[1]-1),
-        y0 = np.random.rand() * (self.shape[1]-1),
-            source = self.gaussianpsf(flux,x,y,x0,y0,self.psfsigma)
-            self.image += source
-
-        
-        psf = self.fluxes[None,None,:] * np.exp(-0.5 * ((xgrid[:,:,None]-self.x0s[None,None,:]) ** 2 + (ygrid[:,:,None]-self.y0s[None,None,:]) ** 2)
-                        / psfsigma ** 2) / np.sqrt(2. * np.pi * psfsigma ** 2)
-
+        t0 = time.time()
+        for i in range(self.N):
+            ind = (x<=self.x0s[i] + self.window*self.psfsigma) & (x>=self.x0s[i] - self.window*self.psfsigma) & \
+                (y<=self.y0s[i] + self.window*self.psfsigma) & (y>=self.y0s[i] - self.window*self.psfsigma)
+            self.image[ind] += self.fluxes[i] * np.exp(-0.5 * ((x[ind]-self.x0s[i]) ** 2 +
+                                                        (y[ind]-self.y0s[i]) ** 2)
+                        / self.psfsigma ** 2) / np.sqrt(2. * np.pi * self.psfsigma ** 2)
+        print time.time()-t0
 
     def draw_fluxes(self):
         r = np.random.rand(self.N)
@@ -61,24 +61,20 @@ class FakeField(object):
         # draw fluxes
         # draw x0s and y0s from uniforms.
 
+        self.draw_fluxes()
+
+        # this needs to go outside of image!!
+        self.x0s = np.random.rand(self.N) * (self.shape[0] - 1)
+        self.y0s = np.random.rand(self.N) * (self.shape[1] - 1)
+
     def make_image(self):
         
         self.shape = self.flat.shape
         self.image = np.zeros(self.shape)
 
-        nx, ny = self.shape
-        x, y = np.meshgrid(range(nx),range(ny))
+        self.add_gaussianpsfs()
 
-        self.draw_fluxes()
-
-        for i in range(self.N):
-            flux = self.fluxes[i]
-            x0 = np.random.rand() * (self.shape[0]-1),
-            y0 = np.random.rand() * (self.shape[1]-1),
-            source = self.gaussianpsf(flux,x,y,x0,y0,self.psfsigma)
-            self.image += source
-
-        self.true = self.image
+        self.true = self.image.copy()
         self.var = self.uninoise**2 + self.noisefact**2 * self.true
         self.image *= self.flat
         self.image += self.dark
@@ -90,6 +86,9 @@ if __name__=='__main__':
     import matplotlib.pyplot as pl
     import pyfits as pf
 
+    # some magic numbers here
+    # gives ~25 S/N > 10 stars per
+    # 10k pixels
     side = 500
     flat = np.ones(side**2).reshape((side,side))
     flat = np.linspace(0.90,1.1,side)
@@ -100,14 +99,16 @@ if __name__=='__main__':
                   0.03,
                   0.03,
                   psfsigma=hwhm,
-                  Nrange=(150,300),
-                  fluxrange=(1,1.5))
-    #pl.imshow(f.image,interpolation='nearest')
-    #pl.gray()
-    #pl.show()
+                  Nmean=1650,
+                  fluxrange=(0.02,8.0))
 
-    print f.fluxes**2/f.uninoise**2/(2.35 * f.psfsigma)**2
+    q = np.sqrt(f.fluxes**2/f.uninoise**2/(2.35 * f.psfsigma)**2)
+    ind = q > 10
+    print q[ind]
+    print q[ind].shape
     print f.fluxes.shape
 
     hdu = pf.PrimaryHDU(f.image)
-    hdu.writeto('foo.fits')
+    hdu.writeto('fooimage.fits')
+    hdu = pf.PrimaryHDU(f.true)
+    hdu.writeto('footrue.fits')
