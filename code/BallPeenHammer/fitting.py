09744a6f4e7e0483dbe8fa0704f0e812a3a21dd8
Ross Fadely <rossfadely@gmail.com>
2014-01-15 17:56:44 +0000

    working psf and flat updates

--- code/BallPeenHammer/fitting.py
+++ code/BallPeenHammer/fitting.py
@@ -1,11 +1,12 @@
 import numpy as np
-import matplotlib.pyplot as pl
 
 from .generation import render_psfs
 
-def PatchFitter(data, ini_psf, ini_flat, detector_grid, psf_grid,
+from scipy.optimize import fmin_bfgs
+
+def PatchFitter(data, ini_psf, ini_flat, patch_grid, psf_grid,
                 patch_centers, background='linear',
-                sequence=['shifts', 'flat', 'psf'], tol=1.e-4,
+                sequence=['shifts', 'flat', 'psf'], tol=1.e-4, eps=1.e-2,
                 shifts=None):
     """
     Patch fitting routines for BallPeenHammer.
@@ -22,22 +23,76 @@ def PatchFitter(data, ini_psf, ini_flat, detector_grid, psf_grid,
 
     while True:
         for kind in sequence:
+            if kind == 'psf':
+                current_psf = update_psf(data, current_flat, current_psf, 
+                                         psf_grid, patch_grid, patch_centers,
+                                         shifts, background, eps)
             if kind == 'flat':
+                assert 0
                 current_flat, counts, sqe = update_flat(data, current_flat,
                                                         rendered_psfs,
-                                                        detector_grid,
+                                                        patch_grid,
                                                         patch_centers,
                                                         background, 
                                                         psf_model=current_psf,
                                                         shifts=shifts,
-                                                        psf_grid=psf_grid)
+                                                        psf_grid=psf_grid,
+                                                        tol=tol)
 
         
         return current_flat, current_psf
 
-def update_flat(data, current_flat, rendered_psfs, detector_grid,
+def update_psf(data, current_flat, current_psf, psf_grid, patch_grid, 
+               patch_centers, shifts, background, eps):
+    """
+    Update the psf model, using bfgs.
+    """
+    p0 = current_psf.ravel().copy()
+    return fmin_bfgs(psf_loss, p0, args=(data, current_flat, psf_grid, 
+                                         patch_grid, patch_centers, 
+                                         shifts, background, eps),
+                     maxiter=1)
+
+
+def psf_loss(psf_model, data, current_flat, psf_grid, patch_grid,
+             patch_centers, shifts, background, eps):
+    """
+    Evaluate the current psf model, given a flat and shifts.
+    """
+    D = np.sqrt(psf_model.size)
+    psf_model = psf_model.reshape(D, D)
+    rendered_psfs = render_psfs(psf_model, shifts, data.shape, psf_grid[0],
+                                psf_grid[1])
+
+    patch_shape = (data.shape[1], data.shape[2])
+    xpg, ypg = patch_grid[0], patch_grid[1]
+    xpc, ypc = patch_centers[0], patch_centers[1]
+
+    # compute squared error
+    sqe = 0.0
+    for i in range(data.shape[0]):
+        psf = rendered_psfs[i]
+        flat = current_flat[xpg + xpc[i], ypg + ypc[i]]
+        flux, bkg_parms, bkg = fit_single_patch(data[i].ravel(),
+                                                psf.ravel(),
+                                                flat.ravel(),
+                                                background)
+        model = flat * (flux * psf + bkg.reshape(patch_shape))
+        sqe += np.sum((data[i] - model) ** 2.)
+
+    # regularization
+    sqg = np.gradient(psf_model)
+    sqg = np.append(sqg[0].ravel(), sqg[1].ravel())
+    sqg = sqg ** 2.
+    reg = np.sum(eps * sqg / sqg.mean())
+    
+    print 'SQE = %0.3f, REG = %0.3f' % (sqe, reg)
+
+    return sqe + reg
+
+def update_flat(data, current_flat, rendered_psfs, patch_grid,
                 patch_centers, background, psf_model=None, shifts=None,
-                psf_grid=None, tol=1.e-4):
+                psf_grid=None, tol=1.e-3):
     """
     Update the flat field given current psf model
     """
@@ -50,53 +105,62 @@ def update_flat(data, current_flat, rendered_psfs, detector_grid,
                                     psf_grid[1])
 
     # shorthand
-    xdg, ydg = detector_grid[0], detector_grid[1]
+    patch_shape = (data.shape[1], data.shape[2])
+    xpg, ypg = patch_grid[0], patch_grid[1]
     xpc, ypc = patch_centers[0], patch_centers[1]
-
+    
     # infer flat
-    for dummy in range(3):
-        sqe = 0.0
+    tse = np.Inf
+    while True:
+        new_tse = 0.0
         norm = np.zeros_like(current_flat)
         new_flat = np.zeros_like(current_flat)
         for i in range(data.shape[0]):
-            cf = current_flat[xdg + xpc[i], ydg + ypc[i]].copy()
-            psf = rendered_psfs[i].copy()
-            amp, bkg_parms, bkg = fit_single_patch(data[i].ravel().copy() / 
-                                                   cf.ravel(), psf.ravel(),
-                                                   background)
-            model = psf * amp * cf
-            sqe += np.sum((data[i].copy() - model) ** 2.)
+            psf = rendered_psfs[i]
+            flat = current_flat[xpg + xpc[i], ypg + ypc[i]]
+            flux, bkg_parms, bkg = fit_single_patch(data[i].ravel(),
+                                                    psf.ravel(),
+                                                    flat.ravel(),
+                                                    background)
+            model = flat * (flux * psf + bkg.reshape(patch_shape))
+            se = (data[i] - model) ** 2.
+            norm[xpg + xpc[i], ypg + ypc[i]] += 1.
+            new_flat[xpg + xpc[i], ypg + ypc[i]] += (data[i] / model) * \
+                current_flat[xpg + xpc[i], ypg + ypc[i]]
+            new_tse += se.sum()
 
-            norm[xdg + xpc[i], ydg + ypc[i]] += 1.
-            new_flat[xdg + xpc[i], ydg + ypc[i]] += (data[i] / model) - cf
-    
-        print sqe
         new_flat /= norm
-        #new_flat *= new_flat.size / new_flat.sum()
-        current_flat += new_flat.copy()
+        new_flat *= new_flat.size / new_flat.sum()
 
-    return new_flat, norm, sqe
+        print tse, new_tse
+        if (tse - new_tse) / new_tse > tol:
+            tse = new_tse
+            current_flat = new_flat
+        else:
+            return new_flat, norm, new_tse
 
-def fit_single_patch(data, psf, background):
+def fit_single_patch(data, psf, flat, background):
     """
     Fit a single patch, return the scale for the psf plus any
     background parameters.  Takes in flattened values for
     data and psf.
     """
+    # not sure why this `* flat` nonsense is the only 
+    # mode that works!
     if background is None:
-        A = np.atleast_2d(psf).T
+        A = np.atleast_2d(psf * flat).T
     elif background is 'constant':
         assert 0, 'this or transpose???'
-        A = np.vstack((psf, np.ones_like(psf)))
+        A = np.vstack((psf * flat, np.ones_like(psf) * flat))
     elif background is 'linear':
-        A = np.vstack((psf, np.ones_like(psf),
-                       np.arange(psf.size), np.arange(psf.size)))
+        A = np.vstack((psf * flat, np.ones_like(psf) * flat,
+                       np.arange(psf.size) * flat,
+                       np.arange(psf.size) * flat))
     else:
         assert False, 'Background model not supported.'
 
     rh = np.dot(A.T, data)
     lh = np.linalg.inv(np.dot(A.T, A))
-    
     parms = np.dot(lh, rh)
     bkg = np.zeros_like(data)
 
@@ -107,50 +171,3 @@ def fit_single_patch(data, psf, background):
         return parms[0], parms[1:], bkg
     else:
         return parms[0], None, bkg
-
-"""
-
-    tse = 1.e200
-    count = 0
-    while True:
-        new_tse = 0.0
-        print count, tse, new_tse
-        #norm = np.zeros_like(current_flat)
-        #new_flat = np.zeros_like(current_flat)
-        count += 1.
-        for i in range(data.shape[0]):
-            # shorthand
-            cf = current_flat[xdg + xpc[i], ydg + ypc[i]].copy()
-            #psf = rendered_psfs[i]
-            #cal_data = data[i].copy().ravel() / cf.ravel()
-
-            # fit the model
-            #amp, bkg_parms, bkg = fit_single_patch(data[i].ravel().copy() * current_flat[xdg + xpc[i], ydg + ypc[i]].ravel().copy(), rendered_psfs[i].ravel().copy(), background)
-            model = rendered_psfs[i].copy() * count #+ bkg.reshape(data[i].shape[0],
-                                            #data[i].shape[1])
-            #model *= cf
-            se = (data[i].copy() - model) ** 2.
-            #norm[xdg + xpc[i], ydg + ypc[i]] += 1
-            #new_flat[xdg + xpc[i], ydg + ypc[i]] += (data[i] / model)
-            new_tse += se.sum()
-        print count, current_flat[xdg + xpc[i], ydg + ypc[i]].ravel()
-        #count = count + 1
-        # scale, sum to one.
-        #new_flat /= norm
-        #new_flat *= new_flat.size / new_flat.sum()
-
-        print 'end', count, tse, new_tse
-        print 
-        tse = 1. * new_tse
-        #print (tse - new_tse) / new_tse
-        # assess convergence
-        #if (tse - new_tse) / new_tse > tol:
-            #tse = new_tse.copy()
-            #current_flat = new_flat
-            #print tse, new_tse
-            #print (tse - new_tse) / new_tse
-        #    print 
-        #else:
-        #    return new_flat, norm, new_tse
-
-"""
