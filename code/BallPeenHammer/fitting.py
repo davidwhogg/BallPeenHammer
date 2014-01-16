2c2fad7aacd53bb0c70111c53545551a1f80ab04
Ross Fadely <rossfadely@gmail.com>
2014-01-16 18:56:50 +0000

    multiprocessing, shift inference

--- code/BallPeenHammer/fitting.py
+++ code/BallPeenHammer/fitting.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import numpy as np
 
 from .generation import render_psfs
@@ -7,28 +8,31 @@ from scipy.optimize import fmin_bfgs
 def PatchFitter(data, ini_psf, ini_flat, patch_grid, psf_grid,
                 patch_centers, background='linear',
                 sequence=['shifts', 'flat', 'psf'], tol=1.e-4, eps=1.e-2,
-                shifts=None):
+                ini_shifts=None):
     """
     Patch fitting routines for BallPeenHammer.
     """
-    assert 'shifts' not in sequence, 'shift inference not yet supported'
-    assert 'flat' in sequence, 'fitting for flat, should be in sequence'
     assert background in [None, 'constant', 'linear']
 
     current_psf = ini_psf.copy()
     current_loss = np.Inf
     current_flat = ini_flat.copy()
+    if ini_shifts is not None:
+        shifts = ini_shifts.copy()
 
     rendered_psfs = None
 
     while True:
         for kind in sequence:
+            if kind == 'shifts':
+                shifts = update_shifts(data, current_flat, current_psf,
+                                       psf_grid, patch_grid, patch_centers,
+                                       shifts, background)
             if kind == 'psf':
                 current_psf = update_psf(data, current_flat, current_psf, 
                                          psf_grid, patch_grid, patch_centers,
                                          shifts, background, eps)
             if kind == 'flat':
-                assert 0
                 current_flat, counts, sqe = update_flat(data, current_flat,
                                                         rendered_psfs,
                                                         patch_grid,
@@ -41,6 +45,72 @@ def PatchFitter(data, ini_psf, ini_flat, patch_grid, psf_grid,
 
         
         return current_flat, current_psf
+
+def update_shifts(data, current_flat, current_psf, psf_grid, patch_grid, 
+                  patch_centers, shifts, background, threads=6):
+    """
+    Update the estimate of the subpixel shifts, given current psf and flat.
+    """
+    xpg, ypg = patch_grid[0], patch_grid[1]
+    xpc, ypc = patch_centers[0], patch_centers[1]
+
+    sqe = np.zeros(data.shape[0])
+    if threads == 1:
+        for i in range(data.shape[0]):
+            p0 = shifts[i]
+            flat = current_flat[xpg + xpc[i], ypg + ypc[i]]
+            res = fmin_bfgs(shift_loss, p0, full_output=True, disp=False,
+                            args=(current_psf, data[i], flat, psf_grid, 
+                                  background))
+            shifts[i] = res[0]
+            sqe[i] = res[1]
+            print i
+    else:
+        mapfn = multiprocessing.Pool(threads).map
+        argslist = [None] * data.shape[0]
+        for i in range(data.shape[0]):
+            flat = current_flat[xpg + xpc[i], ypg + ypc[i]]
+            argslist[i] = [shifts[i], current_psf, data[i], flat, psf_grid,
+                           background]
+
+        results = list(mapfn(update_single_shift, [args for args in argslist]))
+        for i in range(data.shape[0]):
+            shifts[i] = results[i][0]
+            sqe[i] = results[i][1]
+    print sqe
+    print sqe.sum()
+    assert 0
+
+def update_single_shift(*args):
+    """
+    Update a single shift, enabling multiprocessing
+    """
+    args = args[0]
+    p0 = args[0]
+    current_psf = args[1]
+    datum = args[2]
+    flat = args[3]
+    psf_grid = args[4]
+    background = args[5]
+    res = fmin_bfgs(shift_loss, p0, full_output=True, disp=False,
+                    args=(current_psf, datum, flat, psf_grid, 
+                          background))
+    return res
+
+def shift_loss(shift, psf_model, data, flat, psf_grid, background):
+    """
+    Evaluate the shift for a given patch.
+    """
+    shape = (1, data.shape[0], data.shape[1])
+    psf = render_psfs(psf_model, np.array([shift]), shape, psf_grid[0],
+                      psf_grid[1])
+    flux, bkg_parms, bkg = fit_single_patch(data.ravel(),
+                                            psf.ravel(),
+                                            flat.ravel(),
+                                            background)
+    model = flat * (flux * psf + bkg.reshape(data.shape))
+    sqe = np.sum((data - model) ** 2.)
+    return sqe    
 
 def update_psf(data, current_flat, current_psf, psf_grid, patch_grid, 
                patch_centers, shifts, background, eps):
@@ -149,23 +219,24 @@ def fit_single_patch(data, psf, flat, background):
     # mode that works!
     if background is None:
         A = np.atleast_2d(psf * flat).T
-    elif background is 'constant':
-        assert 0, 'this or transpose???'
-        A = np.vstack((psf * flat, np.ones_like(psf) * flat))
-    elif background is 'linear':
+    elif background == 'constant':
+        A = np.vstack((psf * flat, np.ones_like(psf) * flat)).T
+    elif background == 'linear':
+        N = np.sqrt(psf.size).astype(np.int)
+        x, y = np.meshgrid(range(N), range(N))
         A = np.vstack((psf * flat, np.ones_like(psf) * flat,
-                       np.arange(psf.size) * flat,
-                       np.arange(psf.size) * flat))
+                       x.ravel() * flat, y.ravel() * flat)).T
     else:
-        assert False, 'Background model not supported.'
+        assert False, 'Background model not supported: %s' % background
 
     rh = np.dot(A.T, data)
     lh = np.linalg.inv(np.dot(A.T, A))
     parms = np.dot(lh, rh)
     bkg = np.zeros_like(data)
 
+
     if background is not None:
-        for i in range(A.shape[0] - 1):
+        for i in range(A.shape[1] - 1):
             bkg += A[:, i + 1] * parms[i + 1]
         
         return parms[0], parms[1:], bkg
---
+++ code/fit_one_psf.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pyfits as pf
+
+from data_manage.db_utils import get_data
+from BallPeenHammer.fitting import PatchFitter
+
+# get data from a region on the detector
+xn, xx = 495, 519
+yn, yx = xn, xx 
+detector_size = xx - xn
+try:
+    dq = np.loadtxt('../data/dq_%d_%d_%d_%d.dat' % (xn, xx, yn, yx))
+    data = np.loadtxt('../data/data_%d_%d_%d_%d.dat' % (xn, xx, yn, yx))
+except:
+    data_dict = get_data(xn, xx, yn, yx)
+    data = data_dict['pixels'] - data_dict['persist']
+    np.savetxt('../data/dq_%d_%d_%d_%d.dat' % (xn, xx, yn, yx), data_dict['dq'])
+    np.savetxt('../data/data_%d_%d_%d_%d.dat' % (xn, xx, yn, yx), data)
+
+patch_shape = (5, 5)
+dq = dq.reshape(dq.shape[0], patch_shape[0], patch_shape[1])
+data = data.reshape(dq.shape[0], patch_shape[0], patch_shape[1])
+
+# initialize to tinytim
+f = pf.open('../psfs/tinytim-pixelconvolved-507-507.fits') # shape (41, 41)
+ini_psf = f[0].data.T
+f.close()
+
+# random patch centers (actual not needed if not fitting flat)
+   # assign positions                                                           
+xsize = (data.shape[1] - 1) / 2.
+ysize = (data.shape[2] - 1) / 2.
+xc = np.random.randint(detector_size - 2 * xsize, size=data.shape[0])
+yc = np.random.randint(detector_size - 2 * ysize, size=data.shape[0])
+xc += xsize
+yc += ysize
+patch_centers = (xc, yc)
+
+# psf_grid defs
+xg = np.linspace(-0.5 * patch_shape[0], 0.5 * patch_shape[0],
+                  ini_psf.shape[0])
+yg = xg.copy()
+psf_grid = (xg, yg)
+
+# define patch_grid
+yp, xp = np.meshgrid(np.linspace(-ysize, ysize,
+                                  data.shape[2]).astype(np.int),
+                     np.linspace(-xsize, xsize,
+                                  data.shape[1]).astype(np.int))
+patch_grid = (xp, yp)
+
+ini_flat = np.ones((detector_size, detector_size))
+PatchFitter(data, ini_psf, ini_flat, patch_grid, psf_grid,
+            patch_centers, background='linear', sequence=['shifts'],
+            ini_shifts=np.zeros((data.shape[0], 2)))
